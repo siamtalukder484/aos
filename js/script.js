7697d63b7aaa6eda8ce809ac2f8a942c2df148ce
siamtalukder484 <siamtalukder484@gmail.com>
2023-07-25 17:20:31 +0600

    clean code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-// var b = element.scrollHeight - element.clientHeight;
-// console.log(b);
-
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    // console.log(scroll)
-});
-
-// if document.body.scrollTop <= 0 {
-//     alert ("scrolling down")
-// } else {
-//     alert ("scrolling up")
-// }
-
-
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){ 
-   var scrolling = window.pageYOffset || document.documentElement.scrollTop; 
+   var scrolling = window.scrollY 
+
    if (scrolling > lastScrollTop) {
       console.log("scroll down");
    } else if (scrolling < lastScrollTop) {
       console.log("scroll up");
    }
+
    lastScrollTop = scrolling <= 0 ? 0 : scrolling; 
+
 }, false);
+
+
+
+
+$(".nav_link").click(function(){
+    $(this).addClass("active").siblings().removeClass("active")
+})
